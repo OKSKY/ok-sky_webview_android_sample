b8b9a10dbae106c52de30a21779563cfe5c3bf4b
noda-solairo <noda@solairo.co.jp>
2016-07-14 19:28:25 +0900

    first commit

---
+++ app/src/main/java/solairo/oksky/MainActivity.java
@@ -0,0 +1,374 @@
+package solairo.oksky;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int FILE_CHOOSER_RESULT_CODE = 1;
+    private ValueCallback<Uri> mUploadMessage;
+    private ValueCallback<Uri[]> mFilePathCallback;
+
+    private static Map<String, String> extensionMap = new HashMap<String, String>();
+    static {
+        // image
+        extensionMap.put("image/jpeg", "jpg");
+        extensionMap.put("image/png", "png");
+        extensionMap.put("image/gif", "gif");
+
+        // video
+        extensionMap.put("video/x-m4v", "m4v");
+        extensionMap.put("video/mp4", "mp4");
+        extensionMap.put("video/webm", "webm");
+        extensionMap.put("video/ogg", "ogv");
+        extensionMap.put("video/quicktime", "qt");
+        extensionMap.put("application/mp4", "mp4");
+
+        // audio
+        extensionMap.put("audio/mpeg", "mpga");
+        extensionMap.put("audio/mp3", "mp3");
+        extensionMap.put("audio/mp4", "mp4a");
+        extensionMap.put("audio/x-m4a", "m4a");
+        extensionMap.put("audio/x-wav", "wav");
+        extensionMap.put("audio/wav", "wav");
+        extensionMap.put("audio/ogg", "oga");
+        extensionMap.put("audio/webm", "weba");
+        extensionMap.put("audio/amr", "amr");
+        extensionMap.put("audio/AMR", "amr");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://solairo-co.ok-sky.com/customers/new");
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                openFileChooser(uploadMsg, "", "");
+            }
+
+            public void openFileChooser( ValueCallback uploadMsg, String acceptType ) {
+                openFileChooser(uploadMsg, "", "");
+            }
+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture){
+                if (Build.VERSION.SDK_INT < 19) {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setMessage("ご使用のAndroidのバージョンでは、ファイルアップロード機能をサポートしていません。")
+                            .setPositiveButton("OK", null)
+                            .show();
+
+                } else {
+
+                    mUploadMessage = uploadMsg;
+                    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    intent.setType("*/*");
+
+                    Intent chooser = Intent.createChooser(intent, "File Chooser");
+                    MainActivity.this.startActivityForResult(chooser, FILE_CHOOSER_RESULT_CODE);
+
+                }
+
+            }
+
+            public boolean onShowFileChooser(WebView webView,
+                                             ValueCallback<Uri[]> filePathCallback,
+                                             WebChromeClient.FileChooserParams fileChooserParams) {
+
+                if( mFilePathCallback != null) {
+                    mFilePathCallback.onReceiveValue(null);
+                    mFilePathCallback = null;
+                }
+                mFilePathCallback = filePathCallback;
+                Intent intent = fileChooserParams.createIntent();
+                try {
+                    MainActivity.this.startActivityForResult(intent, FILE_CHOOSER_RESULT_CODE);
+                } catch (ActivityNotFoundException e) {
+                    mFilePathCallback = null;
+                    return false;
+                }
+                return true;
+            }
+
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent intent) {
+
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (resultCode != Activity.RESULT_OK || intent == null) {
+            mUploadMessage = null;
+            mFilePathCallback = null;
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mFilePathCallback.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+            mFilePathCallback = null;
+        } else {
+            Uri originalUri = intent.getData();
+
+            String path = getPath(this, originalUri);
+
+            if (path == null) {
+                mUploadMessage.onReceiveValue(Uri.parse(""));
+                mUploadMessage = null;
+                return;
+            }
+
+            File file = new File(path);
+            final int takeFlags = intent.getFlags()
+                    & (Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            getContentResolver().takePersistableUriPermission(originalUri, takeFlags);
+            mUploadMessage.onReceiveValue(Uri.fromFile(file));
+            mUploadMessage = null;
+        }
+
+    }
+
+    /**
+     * Get a file path from a Uri. This will get the the path for Storage Access
+     * Framework Documents, as well as the _data field for the MediaStore and
+     * other file-based ContentProviders.
+     *
+     * @param context The context.
+     * @param uri The Uri to query.
+     * @author paulburke
+     */
+    private String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                return getSDcardDirectoryPath() + "/" + split[1];
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+
+            else if (isGoogleDriveUri(uri)) {
+
+                InputStream is = null;
+
+                try {
+
+                    is = getContentResolver().openInputStream(uri);
+
+                    final File cacheDir = context.getCacheDir();
+                    cacheDir.mkdirs();
+
+                    final String type = context.getContentResolver().getType(uri);
+
+                    Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                    int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                    cursor.moveToFirst();
+                    String fileName = cursor.getString(nameIndex);
+
+                    int lastPeriodIndex = fileName.lastIndexOf(".");
+                    if (lastPeriodIndex != -1) {
+                        fileName = fileName.substring(0, lastPeriodIndex);
+                    }
+
+                    String extension = extensionMap.get(type);
+                    if (extension != null && lastPeriodIndex != -1) {
+                        fileName += ("." + extension);
+                    }
+
+                    File out = new File(cacheDir, fileName);
+
+                    FileUtils.copyInputStreamToFile(is, out);
+
+                    return Uri.fromFile(out).getPath();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (is != null) {
+                        try {
+                            is.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+            }
+        }
+
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri)) {
+                return uri.getLastPathSegment();
+            }
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context The context.
+     * @param uri The Uri to query.
+     * @param selection (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    private String getDataColumn(Context context, Uri uri, String selection,
+                                String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    private static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    private static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    private static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    private static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Drives.
+     */
+    private static boolean isGoogleDriveUri(Uri uri) {
+        return "com.google.android.apps.docs.storage".equals(uri.getAuthority());
+    }
+
+    private String getSDcardDirectoryPath() {
+        return System.getenv("SECONDARY_STORAGE");
+    }
+
+}
